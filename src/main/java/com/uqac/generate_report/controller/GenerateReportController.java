18573eb4ca6348fa9b85446ec4fbba355d1a64f2
SofianeElNaggar <sofiane.elnaggar@gmail.com>
2025-03-15 17:24:36 -0400

    migration

---
+++ src/main/java/com/uqac/generate_report/controller/GenerateReportController.java
@@ -0,0 +1,17 @@
+package com.uqac.generate_report.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reportGenerate")
+@RequiredArgsConstructor
+public class GenerateReportController {
+
+}
