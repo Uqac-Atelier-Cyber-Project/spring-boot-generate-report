a4684501c61fa94034ee72d336d53c684559f191
SofianeElNaggar <sofiane.elnaggar@gmail.com>
2025-03-15 16:13:48 -0400

    commit init

---
+++ src/main/java/com/uqac/generate_report/GenerateReportApplication.java
@@ -0,0 +1,13 @@
+package com.uqac.generate_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uqac/generate_report/GenerateReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uqac.generate_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
